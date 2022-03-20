e7c33b76ae0cc9d8229ad8310c22b914cf99888d
Azhar Nian <azharnian@gmail.com>
2022-03-20 11:25:24 +0700

    [Initial] Mulai PRoject

---
+++ app.js
@@ -0,0 +1,233 @@
+const { Client } = require('whatsapp-web.js');
+const fs = require('fs');
+const util = require('util');
+const express = require('express');
+const RouteMain = require('./src/http/route-main');
+const QRCode = require('qrcode');
+const bodyParser = require('body-parser');
+const cfg = require('./src/lib/constant');
+
+const app = express();
+const port = 3002;
+const host = "0.0.0.0";
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+var ConnectedClient = [],
+    StreamQRList = [],
+    ReadyClientList = [];
+
+const routeMain = new RouteMain();
+routeMain.setBotClient(ConnectedClient);
+routeMain.setQRModule(QRCode);
+
+app.get('/api/registration', async function (req, res) {
+    var querySTR = req.query;
+    if (typeof querySTR.phone == 'undefined') {
+        res.setHeader('Content-Type', 'Application/Json');
+        res.send(JSON.stringify({
+            info: false,
+            status_code: cfg.status_code.MISSING_REQUIRED_ARGS,
+            status: 'Phone number is required to registration.'
+        }));
+
+        return;
+    }
+
+    var USER_ID = generateID(querySTR.phone);
+    if (typeof ConnectedClient[USER_ID] != 'undefined') {
+        res.setHeader('Content-Type', 'Application/Json');
+        res.send(JSON.stringify({
+            info: false,
+            status_code: cfg.status_code.CLIENT_IS_REGISTERED,
+            status: 'Client is already registered.'
+        }));
+
+        return;
+    }
+
+    console.log('registering new service : ' + querySTR.phone);
+
+    StreamQRList[USER_ID] = null;
+    var SESSION_FILE_PATH = './session/botsession-' + USER_ID + '.json';
+    let sessionCfg;
+    if (fs.existsSync(SESSION_FILE_PATH)) {
+        console.log('loading session from storage : ' + querySTR.phone);
+        sessionCfg = require(SESSION_FILE_PATH);
+    }
+
+    ConnectedClient[USER_ID] = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] }, session: sessionCfg });
+    ConnectedClient[USER_ID].on('disconnected', (reason) => {
+        if (fs.existsSync(SESSION_FILE_PATH)) {
+            // remove current session file
+            fs.unlinkSync(SESSION_FILE_PATH);
+            sessionCfg = null;
+        }
+
+        ReadyClientList = ReadyClientList.splice(USER_ID);
+        routeMain.isClientReady(ReadyClientList);
+
+        // delete from connected list
+        removeClient(USER_ID);
+        routeMain.setBotClient(ConnectedClient);
+
+        //destroy client instance
+        ConnectedClient[USER_ID].destroy().then(function () {
+            console.log(ConnectedClient[USER_ID]);
+            console.log('Client is shutdown..');
+        });
+    });
+
+    ConnectedClient[USER_ID].on('authenticated', (session) => {
+        console.log('login sucess..');
+        sessionCfg = session;
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+
+    ConnectedClient[USER_ID].on('auth_failure', msg => {
+        // Fired if session restore was unsuccessfull
+        console.error('AUTHENTICATION FAILURE', msg);
+
+        // remove current session file
+        fs.unlinkSync(SESSION_FILE_PATH);
+        sessionCfg = null;
+
+        //destroy client instant 
+        ConnectedClient[USER_ID].destroy().then(function () {
+            console.log('Client is shutdown..');
+
+            // delete from connected list
+            removeClient(USER_ID);
+            routeMain.setBotClient(ConnectedClient);
+        });
+    });
+
+    ConnectedClient[USER_ID].on('qr', (qr) => {
+        // Generate and scan this code with your phone
+        StreamQRList[USER_ID] = qr;
+        routeMain.setQRCode(StreamQRList);
+        console.log("qrcode is ready..");
+    });
+
+    ConnectedClient[USER_ID].on('ready', () => {
+        StreamQRList[USER_ID] = false;
+        routeMain.setQRCode(StreamQRList);
+
+        ReadyClientList[USER_ID] = true;
+        routeMain.isClientReady(ReadyClientList);
+        console.log('Client is ready..');
+    });
+
+    ConnectedClient[USER_ID].on('message', async msg => {
+        console.log(util.format("incoming message from %s : %s", msg.from, msg.body));
+        if (msg.body == '!ping') {
+            const chat = await msg.getChat();
+            chat.sendStateTyping();
+            client.sendMessage(msg.from, 'pong');
+            chat.clearState();
+        }
+    });
+
+    //start whatsapp client engine 
+    console.log('initializing');
+    var registeringOK = false;
+    var message = "Registration Success";
+    try {
+        await ConnectedClient[USER_ID].initialize();
+        registeringOK = true;
+    } catch (error) {
+        message = "Unable to register client, failed to connect to the host.";
+    }
+
+    if (registeringOK) {
+        //update botclient object
+        routeMain.setBotClient(ConnectedClient);
+    } else {
+        removeClient(USER_ID);
+    }
+
+    res.setHeader('Content-Type', 'Application/Json');
+    res.send(JSON.stringify({
+        info: registeringOK,
+        status: message
+    }));
+});
+
+//define route request
+//send text message, media message, see: https://github.com/barokurniawan/wabot-service/blob/master/README.md
+app.get('/api/message', routeMain.handleIndex.bind(routeMain));
+app.post('/api/message', routeMain.handleIndex.bind(routeMain));
+
+//get client device information
+app.get('/api/device', routeMain.handleDevice.bind(routeMain));
+app.post('/api/device', routeMain.handleDevice.bind(routeMain));
+
+//get qrcode 
+app.get('/api/qr', routeMain.handleScanner.bind(routeMain));
+
+//reset will remove session so we need to scan new qrcode
+//reset will make some downtime
+app.post('/api/device/reset', function (req, res) {
+    if (fs.existsSync(SESSION_FILE_PATH) == false || routeMain.clientReady == false) {
+        res.setHeader('Content-Type', 'Application/Json');
+        res.send(JSON.stringify({
+            info: false,
+            status: 'Reset a non ready client is illegal.'
+        }));
+
+        return;
+    }
+
+    console.log('Shuting down the client..');
+    // remove current session file
+    fs.unlinkSync(SESSION_FILE_PATH);
+    sessionCfg = null;
+
+    //the best solution for reseting client is reset the container it self
+    process.exit(1);
+});
+
+//use this endpoint to check server status after reseting
+app.get('/api/health', function (req, res) {
+    res.setHeader('Content-Type', 'Application/Json');
+    res.send(JSON.stringify({
+        info: true,
+        status: 'server is up'
+    }));
+});
+
+app.get('/api/list-user', function (req, res) {
+    res.setHeader('Content-Type', 'Application/Json');
+    res.send(JSON.stringify({
+        info: true,
+        data: extractClient(ConnectedClient)
+    }));
+});
+
+function generateID(phone) {
+    return phone + "@c.us";
+}
+
+function extractClient(ConnectedClient) {
+    var o = [];
+    for (k in ConnectedClient) {
+        o.push(k);
+    }
+
+    return o;
+}
+
+function removeClient(USER_ID) {
+    delete ConnectedClient[USER_ID];
+}
+
+//start http server, this server is only visible between container
+//not visible to outside, use interface to get access to the engine
+app.listen(port, host, () => console.log(`listening at http://${host}:${port}`));
---
+++ src/http/route-main.js
@@ -0,0 +1,233 @@
+const { MessageMedia } = require('whatsapp-web.js');
+const cfg = require('../../src/lib/constant');
+const axios = require('axios').default;
+
+const fileToBase64 = async function (file) {
+    let image = await axios.get(file, { responseType: 'arraybuffer' });
+    let returnedB64 = Buffer.from(image.data).toString('base64');
+
+    return returnedB64;
+}
+
+module.exports = class RouteMain {
+
+    constructor() {
+        this.client = null;
+        this.clientReady = [];
+        this._qrcode = [];
+        this.QRModule = null;
+    }
+
+    generateID(phone) {
+        return phone + "@c.us";
+    }
+
+    isClientExists(USER_ID) {
+        return typeof this.client[USER_ID] == 'undefined' ? false : true;
+    }
+
+    isClientReady(status) {
+        this.clientReady = status;
+    }
+
+    setBotClient(botClient) {
+        this.client = botClient;
+    }
+
+    setQRCode(_qrcode) {
+        this._qrcode = _qrcode;
+    }
+
+    setQRModule(QRCode) {
+        this.QRModule = QRCode;
+    }
+
+    getBotClient() {
+        return this.client;
+    }
+
+    getQRCode(cl) {
+        return this._qrcode[cl] == undefined ? null : this._qrcode[cl];
+    }
+
+    getQRModule() {
+        return this.QRModule;
+    }
+
+    async handleIndex(req, res) {
+        var _that = this,
+            data = req.query;
+
+        if (req.method == "POST") {
+            data = req.body;
+        }
+
+        if (typeof data.cl == 'undefined') {
+            res.setHeader("Content-Type", "Application/Json");
+            res.status(200).send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.MISSING_REQUIRED_ARGS,
+                status: "CL is required"
+            }));
+        }
+
+        var USER_ID = _that.generateID(data.cl);
+        if (!_that.isClientExists(USER_ID)) {
+            res.setHeader('Content-Type', 'Application/Json');
+            res.send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_IS_NOT_EXISTS,
+                status: 'Client is not exists'
+            }));
+            return;
+        }
+
+        res.setHeader('Content-Type', 'Application/Json');
+        if (_that.clientReady[USER_ID] == false) {
+            res.send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_IS_NOT_READY,
+                status: 'Client is not ready'
+            }));
+            return;
+        }
+
+        if (typeof data.phone != "undefined") {
+            if (typeof data.mime != "undefined" && typeof data.file != "undefined" && typeof data.filename != "undefined") {
+                const chat = await this.client[USER_ID].getChatById(data.phone + "@c.us");
+                chat.sendStateTyping();
+                var media = new MessageMedia(data.mime, await fileToBase64(data.file), data.filename);
+
+                if (data.message) {
+                    this.client[USER_ID].sendMessage(data.phone + "@c.us", media, {
+                        caption: data.message
+                    });
+                } else {
+                    this.client[USER_ID].sendMessage(data.phone + "@c.us", media);
+                }
+
+                chat.clearState();
+            } else {
+                const chat = await this.client[USER_ID].getChatById(data.phone + "@c.us");
+                chat.sendStateTyping();
+                this.client[USER_ID].sendMessage(data.phone + "@c.us", data.message);
+                chat.clearState();
+            }
+        }
+
+        res.send(JSON.stringify({
+            info: true,
+            status: 'send to queue',
+            data: {
+                message: data.message,
+                target: data.phone
+            }
+        }));
+    }
+
+    handleDevice(req, res) {
+        var state = null,
+            webVersion = null,
+            that = this,
+            querySTR = req.query;
+
+        if (typeof querySTR.cl == 'undefined') {
+            res.setHeader("Content-Type", "Application/Json");
+            res.status(200).send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.MISSING_REQUIRED_ARGS,
+                status: "CL is required"
+            }));
+        }
+
+        var USER_ID = that.generateID(querySTR.cl);
+        if (!that.isClientExists(USER_ID)) {
+            res.setHeader('Content-Type', 'Application/Json');
+            res.send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_IS_NOT_EXISTS,
+                status: 'Client is not exists'
+            }));
+            return;
+        }
+
+        if (that.clientReady[USER_ID] == false) {
+            res.setHeader('Content-Type', 'Application/Json');
+            res.send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_IS_NOT_READY,
+                status: 'Client is not ready'
+            }));
+
+            return;
+        }
+
+        that.client[USER_ID].getState().then(function (result) {
+            state = result;
+            that.client[USER_ID].getWWebVersion().then(function (result) {
+                webVersion = result;
+
+                res.setHeader('Content-Type', 'Application/Json');
+                res.send(JSON.stringify({
+                    info: true,
+                    state: state,
+                    webVersion: webVersion,
+                    device: that.client[USER_ID].info
+                }));
+            });
+        });
+    }
+
+    async handleScanner(req, res) {
+        var _that = this,
+            querySTR = req.query;
+
+        if (typeof querySTR.cl == 'undefined') {
+            res.setHeader("Content-Type", "Application/Json");
+            res.status(200).send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.MISSING_REQUIRED_ARGS,
+                status: "CL is required"
+            }));
+        }
+
+        var USER_ID = _that.generateID(querySTR.cl);
+        if (!_that.isClientExists(USER_ID)) {
+            res.setHeader('Content-Type', 'Application/Json');
+            res.send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_IS_NOT_EXISTS,
+                status: 'Client is not exists'
+            }));
+            return;
+        }
+
+        var qr = _that.getQRCode(USER_ID);
+        if (qr) {
+            var base64 = await _that.getQRModule().toDataURL(qr);
+            res.setHeader("Content-Type", "Application/Json");
+            res.status(200).send(JSON.stringify({
+                info: true,
+                data: {
+                    base64: base64
+                },
+                status_code: cfg.status_code.QRCODE_READY,
+                status: "QR is ready"
+            }));
+        } else if (qr === null) {
+            res.setHeader("Content-Type", "Application/Json");
+            res.status(200).send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_IS_NOT_EXISTS,
+                status: "Device is not registered"
+            }));
+        } else {
+            res.setHeader("Content-Type", "Application/Json");
+            res.status(200).send(JSON.stringify({
+                info: false,
+                status_code: cfg.status_code.CLIENT_CONNECTED,
+                status: "Device is already connected"
+            }));
+        }
+    }
+}
